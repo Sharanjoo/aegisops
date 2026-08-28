0b11516c04bed1938825631664a058a4309df075
Sharan Joseph <sharanjoseph16@gmail.com>
2026-08-28 00:30:44 -0700

    feat(incident-service): scaffold Spring Boot service

---
+++ services/incident-service/src/test/java/io/aegisops/incident/IncidentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aegisops.incident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
